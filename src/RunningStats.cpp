84bb9dc557f8e96d7cd3366daec111faf9740136
Jeff <jeffrey.pollock@smartodds.co.uk>
2016-04-04 23:12:22 +0100

    messing around with magit

--- src/RunningStats.cpp
+++ src/RunningStats.cpp
@@ -1,27 +1,36 @@
-
 #include "kthSmallestNumber.hpp"
 #include "Median.hpp"
 #include <iostream>
 #include <string>
 
-int main() {
-
-	std::string line;
-
-	KthSmallestNumber<int> tenthSmallestNumber(10);
-	Median<int> median;
-
-	while (std::getline(std::cin, line)) {
-
-		int number = stoi(line);
-
-		tenthSmallestNumber.addNumber(number);
-		std::cout << tenthSmallestNumber << std::endl;
-
-		median.addNumber(number);
-		std::cout << median << std::endl;
-
-	}
-
-	return 0;
+#include <gtest/gtest.h>
+TEST(MathTest, TwoPlusTwoEqualsFour) {
+	EXPECT_EQ(2 + 2, 4);
 }
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+//int main() {
+//
+//	std::string line;
+//
+//	KthSmallestNumber<int> tenthSmallestNumber(10);
+//	Median<int> median;
+//
+//	while (std::getline(std::cin, line)) {
+//
+//		int number = stoi(line);
+//
+//		tenthSmallestNumber.addNumber(number);
+//		std::cout << tenthSmallestNumber << std::endl;
+//
+//		median.addNumber(number);
+//		std::cout << median << std::endl;
+//
+//	}
+//
+//	return 0;
+//}
